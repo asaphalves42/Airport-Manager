46407c2614cbe23a542ec91e8ff5207318c02436
David Fernandes <1221777@isep.ipp.pt>
2023-01-22 16:14:19 +0000

    Alteração das funções ler e Gravar Tipulantes e Passageiros.
    Adição do nº de voo na clase passageiro e nº de voo na classe tripulação para podermos fazer filtros de pesquisa por voos
    Correcão da função filtrar voo por rota

--- src/avioes/apoo/app/Passageiros.java
+++ src/avioes/apoo/app/Passageiros.java
@@ -5,13 +5,15 @@ public class Passageiros extends Pessoa {
 	private String email;
 	private String tipoBilhete;
 	private String lugarReservado;
+	private String nVoo;
 	
-	public Passageiros(String id, String nome, String nacionalidade, String morada, int telefone, String dataDeNascimento,
+	public Passageiros(String id, String nVoo, String nome, String nacionalidade, String morada, int telefone, String dataDeNascimento,
 			String email, String tipoBilhete, String lugarReservado) {
 		
 		this.email = email;
 		this.tipoBilhete = tipoBilhete;
 		this.lugarReservado = lugarReservado;
+		this.nVoo = nVoo;
 	}
 
 	public Passageiros() {
@@ -40,6 +42,15 @@ public class Passageiros extends Pessoa {
 
 	public void setLugarReservado(String lugarReservado) {
 		this.lugarReservado = lugarReservado;
+	}
+	
+
+	public String getnVoo() {
+		return nVoo;
+	}
+
+	public void setnVoo(String nVoo) {
+		this.nVoo = nVoo;
 	}
 
 	@Override
